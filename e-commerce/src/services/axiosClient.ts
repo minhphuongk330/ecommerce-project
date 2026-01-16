df22cccc3d1e51afaa57ec1e1a92e521abab708e
phuong <minhphuongk330@gmail.com>
2026-01-16 10:11:59 +0700

    Update API_URL to use env variable

--- e-commerce/src/services/axiosClient.ts
+++ e-commerce/src/services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "~/stores/useAuth";
 
-const API_URL = "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 const axiosClient = axios.create({
 	baseURL: API_URL,
