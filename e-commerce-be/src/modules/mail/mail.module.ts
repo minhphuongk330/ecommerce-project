d9af616426b2700b4e78f2d40c5ac2da5b5e59c7
phuong <minhphuongk330@gmail.com>
2026-01-23 11:39:14 +0700

    refactor

--- e-commerce-be/src/modules/mail/mail.module.ts
+++ e-commerce-be/src/modules/mail/mail.module.ts
@@ -1,16 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
   imports: [
     MailerModule.forRootAsync({
+      imports: [ConfigModule],
       useFactory: async (config: ConfigService) => ({
         transport: {
           host: 'smtp-relay.brevo.com',
-          port: 587,
-          secure: false,
+          port: 465,
+          secure: true,
           auth: {
             user: config.get('MAIL_USER'),
             pass: config.get('MAIL_PASS'),
